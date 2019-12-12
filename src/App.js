99d5574c806b0564bf784682279b1d7b4b97a635
Brian Desilva <brian.desilva@serko.com>
2019-12-12 14:10:35 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useTranslation} from 'react-i18next';
+import i18next from 'i18next';
 
 function App() {
+  const { t } = useTranslation();
+
+  function handleClick(lang){
+  i18next.changeLanguage(lang);
+  }
   return (
     <div className="App">
+      <nav styler = {{width: '100%', padding: '2rem 0', backgroundColour: 'blue'}}>
+        <button onClick={()=>handleClick('en')}>
+          English
+        </button>
+        <button onClick={()=>handleClick('ko')}>
+          Korean
+        </button>
+        <button onClick={()=>handleClick('chi')}>
+          Chinese
+        </button>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{t('MainScreenFirstLine.1')}</p>
+        <p>{t('MainScreenSecondLine.1')}</p>
       </header>
     </div>
   );
