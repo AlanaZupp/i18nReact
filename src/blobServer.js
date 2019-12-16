76c6248c7607f2555f7e7941916080091079e1ac
Alana Zuppicich <48972200+AlanaZupp@users.noreply.github.com>
2019-12-17 10:49:25 +1300

    Fixed connection

---
+++ src/blobServer.js
@@ -0,0 +1,21 @@
+const { BlobServiceClient } = require('@azure/storage-blob');
+const CONNECT_STR = process.env.CONNECT_STR;
+
+ async function main() {
+    // Quick start code goes here
+    // Create the BlobServiceClient object which will be used to create a container client
+    const blobServiceClient = await BlobServiceClient.fromConnectionString(CONNECT_STR);
+    
+    // Create a unique name for the container
+    const containerName = 'languages';
+    const blobName = 'ko/translation.json';
+    
+    // Get a reference to a container
+    const containerClient = await blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    //
+    await blockBlobClient.downloadToFile("../public/locales/ko/translation.json", 0);
+}
+// A helper function used to read a Node.js readable stream into a string
+main().then(() => console.log('Done')).catch((ex) => console.log(ex.message));
